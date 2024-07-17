cf85db6208fd1323dee123165a783d1f97c49bcc
ChaeAg <dydkglel123@gmail.com>
2024-07-18 01:13:10 +0900

    [FIX] DTO 에러 메세지 오타 수정

--- src/main/java/org/routineade/RoutineAdeServer/dto/routine/RoutineUpdateRequest.java
+++ src/main/java/org/routineade/RoutineAdeServer/dto/routine/RoutineUpdateRequest.java
@@ -8,14 +8,14 @@ import java.util.List;
 public record RoutineUpdateRequest(
         @NotBlank(message = "루틴명은 비어 있거나, 공백일 수 없습니다.")
         @NotNull(message = "루틴명은 null일 수 없습니다.")
-        @Size(min = 1, max = 15, message = "피드 내용은 15자를 초과할 수 없습니다.")
+        @Size(min = 1, max = 15, message = "루틴명은 15자를 초과할 수 없습니다.")
         String routineTitle,
         @NotBlank(message = "카테고리는 비어 있거나, 공백일 수 없습니다.")
         @NotNull(message = "카테고리는 1개 이상 선택해야 합니다.")
         String routineCategory,
-        @NotNull(message = "알람 여부는 null일 수 없습니다.")
-        List<String> repeatDays,
         @NotNull(message = "반복 요일은 1개 이상 선택해야 합니다.")
+        List<String> repeatDays,
+        @NotNull(message = "알람 여부는 null일 수 없습니다.")
         Boolean isAlarmEnabled
 ) {
 }
