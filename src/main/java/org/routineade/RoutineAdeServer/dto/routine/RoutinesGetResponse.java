a643ea72d0f3ce3f6caab595979b2a6e505a46f6
ChaeAg <dydkglel123@gmail.com>
2024-07-18 21:09:07 +0900

    [FIX] 루틴 조회 API 사용자 감정 형식 오류 수정

--- src/main/java/org/routineade/RoutineAdeServer/dto/routine/RoutinesGetResponse.java
+++ src/main/java/org/routineade/RoutineAdeServer/dto/routine/RoutinesGetResponse.java
@@ -2,15 +2,17 @@ package org.routineade.RoutineAdeServer.dto.routine;
 
 import java.util.List;
 import org.routineade.RoutineAdeServer.domain.UserEmotion;
+import org.routineade.RoutineAdeServer.domain.common.Emotion;
 
 public record RoutinesGetResponse(
         List<PersonalRoutineGetResponse> personalRoutines,
         List<GroupRoutinesGetResponse> groupRoutines,
-        UserEmotion userEmotion
+        Emotion userEmotion
 ) {
     public static RoutinesGetResponse of(List<PersonalRoutineGetResponse> personalRoutines,
                                          List<GroupRoutinesGetResponse> groupRoutines,
                                          UserEmotion userEmotion) {
-        return new RoutinesGetResponse(personalRoutines, groupRoutines, userEmotion);
+        return new RoutinesGetResponse(personalRoutines, groupRoutines,
+                userEmotion == null ? null : userEmotion.getEmotion());
     }
 }
