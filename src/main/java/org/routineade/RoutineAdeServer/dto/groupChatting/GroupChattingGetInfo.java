e1d2c22f8973005d92a37586347de2a8d9a07da2
ChaeAg <dydkglel123@gmail.com>
2024-07-20 15:23:46 +0900

    [FIX] 그룹 채팅 조회 API에 Response에 누락된 image 컬럼 추가

--- src/main/java/org/routineade/RoutineAdeServer/dto/groupChatting/GroupChattingGetInfo.java
+++ src/main/java/org/routineade/RoutineAdeServer/dto/groupChatting/GroupChattingGetInfo.java
@@ -11,14 +11,16 @@ public record GroupChattingGetInfo(
         boolean isMine,
         Long userId,
         String nickname,
-        String image,
+        String profileImage,
         String content,
+        String image,
         String createdDate
 ) {
     public static GroupChattingGetInfo of(GroupChatting groupChatting, User user) {
         return new GroupChattingGetInfo(Objects.equals(groupChatting.getUser(), user),
                 groupChatting.getUser().getUserId(), groupChatting.getUser().getNickname(),
                 groupChatting.getUser().getProfileImage(), groupChatting.getContent(),
-                groupChatting.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 (E)", KOREAN)));
+                groupChatting.getImage(),
+                groupChatting.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 E", KOREAN)));
     }
 }
