3315f6d8bd07d6e00bfb0c5d17d343a2b575de92
ChaeAg <dydkglel123@gmail.com>
2024-08-20 23:59:34 +0900

    [FIX] 그룹 기본정보 조회 응답값 중 그룹 멤버 userId 도 포함하도록 수정

--- src/main/java/org/routineade/RoutineAdeServer/dto/group/GroupMemberInfo.java
+++ src/main/java/org/routineade/RoutineAdeServer/dto/group/GroupMemberInfo.java
@@ -3,12 +3,13 @@ package org.routineade.RoutineAdeServer.dto.group;
 import org.routineade.RoutineAdeServer.domain.User;
 
 public record GroupMemberInfo(
+        Long userId,
         String nickname,
         String profileImage
 ) {
 
     public static GroupMemberInfo of(User user) {
-        return new GroupMemberInfo(user.getNickname(), user.getProfileImage());
+        return new GroupMemberInfo(user.getUserId(), user.getNickname(), user.getProfileImage());
     }
 
 }
