6874ca2094609b27d6f7b1f629e41bd0d49bedd9
ChaeAg <dydkglel123@gmail.com>
2024-08-27 17:19:38 +0900

    [FIX] 그룹 루틴 조회 시 반복 요일 요일순서대로 정렬하여 응답하도록 수정

--- src/main/java/org/routineade/RoutineAdeServer/dto/group/GroupRoutineDetail.java
+++ src/main/java/org/routineade/RoutineAdeServer/dto/group/GroupRoutineDetail.java
@@ -1,5 +1,7 @@
 package org.routineade.RoutineAdeServer.dto.group;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import org.routineade.RoutineAdeServer.domain.Routine;
 
@@ -8,9 +10,11 @@ public record GroupRoutineDetail(
         String routineTitle,
         List<String> repeatDay
 ) {
+    private static final List<String> DAY_ORDER = Arrays.asList("월", "화", "수", "목", "금", "토", "일");
 
     public static GroupRoutineDetail of(Routine routine, List<String> repeatDays) {
+        // repeatDays 리스트를 요일 순서대로 정렬
+        repeatDays.sort(Comparator.comparingInt(DAY_ORDER::indexOf));
         return new GroupRoutineDetail(routine.getRoutineId(), routine.getRoutineTitle(), repeatDays);
     }
-
 }
