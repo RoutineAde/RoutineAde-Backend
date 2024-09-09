4f2c54367565abb7ff9bf81271d5290f66c267e0
ChaeAg <dydkglel123@gmail.com>
2024-09-09 17:41:58 +0900

    [FIX] 루틴 조회의 그룹 응답 결과에 카테고리 중복 결과 제거

--- src/main/java/org/routineade/RoutineAdeServer/dto/routine/GroupRoutineInfo.java
+++ src/main/java/org/routineade/RoutineAdeServer/dto/routine/GroupRoutineInfo.java
@@ -5,12 +5,10 @@ import org.routineade.RoutineAdeServer.domain.Routine;
 public record GroupRoutineInfo(
         Long routineId,
         String routineTitle,
-        String routineCategory,
         Boolean isCompletion,
         Boolean isAlarmEnabled
 ) {
     public static GroupRoutineInfo of(Routine routine, Boolean isCompletion, Boolean isAlarmEnabled) {
-        return new GroupRoutineInfo(routine.getRoutineId(), routine.getRoutineTitle(),
-                routine.getRoutineCategory().getLabel(), isCompletion, isAlarmEnabled);
+        return new GroupRoutineInfo(routine.getRoutineId(), routine.getRoutineTitle(), isCompletion, isAlarmEnabled);
     }
 }
