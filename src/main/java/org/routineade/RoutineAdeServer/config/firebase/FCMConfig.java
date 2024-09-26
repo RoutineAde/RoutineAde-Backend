ead1d74bc39ed437b5177005478769d44f26c23f
이채은 <109871579+ChaeAg@users.noreply.github.com>
2024-09-26 14:46:16 +0900

    [CHORE] CD.yml 수정

--- src/main/java/org/routineade/RoutineAdeServer/config/firebase/FCMConfig.java
+++ src/main/java/org/routineade/RoutineAdeServer/config/firebase/FCMConfig.java
@@ -1,41 +1,45 @@
-package org.routineade.RoutineAdeServer.config.firebase;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.messaging.FirebaseMessaging;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
-@Configuration
-public class FCMConfig {
-    @Bean
-    FirebaseMessaging firebaseMessaging() throws IOException {
-        ClassPathResource resource = new ClassPathResource(
-                "firebase/routine-ade-firebase-adminsdk-uime1-11ab5526ac.json");
-
-        InputStream refreshToken = resource.getInputStream();
-
-        FirebaseApp firebaseApp = null;
-        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
-
-        if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
-            for (FirebaseApp app : firebaseAppList) {
-                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
-                    firebaseApp = app;
-                }
-            }
-        } else {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
-                    .build();
-            firebaseApp = FirebaseApp.initializeApp(options);
-        }
-
-        return FirebaseMessaging.getInstance(firebaseApp);
-    }
-}
+//package org.routineade.RoutineAdeServer.config.firebase;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import com.google.firebase.messaging.FirebaseMessaging;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.List;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//
+//@Configuration
+//public class FCMConfig {
+//
+//    @Value("${firebase.url}")
+//    String FIRE_BASE_FILE_URL;
+//
+//    @Bean
+//    FirebaseMessaging firebaseMessaging() throws IOException {
+//        ClassPathResource resource = new ClassPathResource(FIRE_BASE_FILE_URL);
+//
+//        InputStream refreshToken = resource.getInputStream();
+//
+//        FirebaseApp firebaseApp = null;
+//        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+//
+//        if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
+//            for (FirebaseApp app : firebaseAppList) {
+//                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+//                    firebaseApp = app;
+//                }
+//            }
+//        } else {
+//            FirebaseOptions options = FirebaseOptions.builder()
+//                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+//                    .build();
+//            firebaseApp = FirebaseApp.initializeApp(options);
+//        }
+//
+//        return FirebaseMessaging.getInstance(firebaseApp);
+//    }
+//}
--- src/main/java/org/routineade/RoutineAdeServer/service/FCMNotificationService.java
+++ src/main/java/org/routineade/RoutineAdeServer/service/FCMNotificationService.java
@@ -1,42 +1,42 @@
-package org.routineade.RoutineAdeServer.service;
-
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
-import lombok.RequiredArgsConstructor;
-import org.routineade.RoutineAdeServer.domain.User;
-import org.routineade.RoutineAdeServer.dto.firebase.FCMNotificationRequest;
-import org.springframework.stereotype.Service;
-
-@RequiredArgsConstructor
-@Service
-public class FCMNotificationService {
-
-    private final FirebaseMessaging firebaseMessaging;
-
-    public void sendNotificationByToken(FCMNotificationRequest request) {
-        User user = request.user();
-
-        if (user.getFirebaseToken() != null) {
-            Notification notification = Notification.builder()
-                    .setTitle(request.title())
-                    .setBody(request.body())
-                    .build();
-
-            Message message = Message.builder()
-                    .setToken(user.getFirebaseToken())
-                    .setNotification(notification)
-                    .build();
-
-            try {
-                firebaseMessaging.send(message);
-            } catch (FirebaseMessagingException e) {
-                throw new IllegalArgumentException("푸시 알림 보내기를 실패했습니다.");
-            }
-        } else {
-            throw new IllegalArgumentException("서버에 저장된 해당 유저의 Firebase Token이 존재하지 않습니다.");
-        }
-    }
-
-}
+//package org.routineade.RoutineAdeServer.service;
+//
+//import com.google.firebase.messaging.FirebaseMessaging;
+//import com.google.firebase.messaging.FirebaseMessagingException;
+//import com.google.firebase.messaging.Message;
+//import com.google.firebase.messaging.Notification;
+//import lombok.RequiredArgsConstructor;
+//import org.routineade.RoutineAdeServer.domain.User;
+//import org.routineade.RoutineAdeServer.dto.firebase.FCMNotificationRequest;
+//import org.springframework.stereotype.Service;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class FCMNotificationService {
+//
+//    private final FirebaseMessaging firebaseMessaging;
+//
+//    public void sendNotificationByToken(FCMNotificationRequest request) {
+//        User user = request.user();
+//
+//        if (user.getFirebaseToken() != null) {
+//            Notification notification = Notification.builder()
+//                    .setTitle(request.title())
+//                    .setBody(request.body())
+//                    .build();
+//
+//            Message message = Message.builder()
+//                    .setToken(user.getFirebaseToken())
+//                    .setNotification(notification)
+//                    .build();
+//
+//            try {
+//                firebaseMessaging.send(message);
+//            } catch (FirebaseMessagingException e) {
+//                throw new IllegalArgumentException("푸시 알림 보내기를 실패했습니다.");
+//            }
+//        } else {
+//            throw new IllegalArgumentException("서버에 저장된 해당 유저의 Firebase Token이 존재하지 않습니다.");
+//        }
+//    }
+//
+//}
