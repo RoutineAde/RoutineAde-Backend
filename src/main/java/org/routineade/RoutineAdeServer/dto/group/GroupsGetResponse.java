0d1815d52c5003cd60cfb4443ecc59d9bbf84bce
ChaeAg <dydkglel123@gmail.com>
2024-10-09 19:48:02 +0900

    [FIX] 그룹 조회 시 그룹 개수 응답에 추가

--- src/main/java/org/routineade/RoutineAdeServer/dto/group/GroupsGetResponse.java
+++ src/main/java/org/routineade/RoutineAdeServer/dto/group/GroupsGetResponse.java
@@ -3,10 +3,11 @@ package org.routineade.RoutineAdeServer.dto.group;
 import java.util.List;
 
 public record GroupsGetResponse(
+        int groupCount,
         List<GroupBasicInfo> groups
 ) {
     public static GroupsGetResponse of(List<GroupBasicInfo> groups) {
-        return new GroupsGetResponse(groups);
+        return new GroupsGetResponse(groups.size(), groups);
     }
 
 }
